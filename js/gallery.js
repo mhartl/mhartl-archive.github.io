c7fc2a527c64268a76fe909b10c3390d5aee2a48
Michael Hartl <michael@michaelhartl.com>
2018-03-17 09:26:01 -0700

    Get gallery working

--- js/gallery.js
+++ js/gallery.js
@@ -2,16 +2,28 @@
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click.
 function activateGallery() {
-  let images = document.querySelector("#gallery-thumbs").
-                        querySelectorAll("img");
-  let currentDisplayImage = document.querySelector("#gallery-photo").
-                                     querySelector("img");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo").
+                           querySelector("img");
 
-  images.forEach(function(image) {
-    image.addEventListener("click", function() {
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
       // Set clicked image as display image.
-      let newImageSrc = image.dataset.largeVersion;
-      currentDisplayImage.setAttribute("src", newImageSrc);
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
